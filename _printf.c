ea3acec1e3b268f617e6d5d456659d5e39bf1472
Arafa <khalafarafa@gmail.com>
2023-11-12 07:20:53 -0800

    001

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _printf - varledic function to print multi-parameter string
+ * @format: the vars
+ *
+ * Return: integer
+ */
+int _printf(const char *format, ...);
+{
+	va_list args;
+	va_start(args, format);
+
+	if (!format)
+		return (-1);
+
+	
+}
