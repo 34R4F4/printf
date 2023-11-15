4b93192dfab88cc8a3c0e8b9a5c8b8e1e090b51d
Arafa <khalafarafa@gmail.com>
2023-11-14 17:51:12 -0800

    022

--- print_format.c
+++ print_format.c
@@ -25,8 +25,8 @@ int print_format(char specifier, va_list args)
 		count += print_digit((unsigned int) (va_arg(args, unsigned int)), 2);
 	else if (specifier == 'u')
 		count += print_digit((unsigned int) (va_arg(args, unsigned int)), 10);
-	else if (specifier == 'b')
-		count += print_digit((long) (va_arg(args, int)), 2);
+	else if (specifier == 'o')
+		count += print_digit((unsigned int) (va_arg(args, unsigned int)), 8);
 	else if (specifier == 'b')
 		count += print_digit((long) (va_arg(args, int)), 2);
 	else if (specifier == '%')
